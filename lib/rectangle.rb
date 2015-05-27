9e3f92c8ac8e324acd923620e12e269c3a54b1a1
wallydrag <debashish.ghatak@gmail.com>
2015-05-27 12:47:57 +0530

    [Deba and Shukla] Rectanle.new with sides

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,26 +1,34 @@
 class Rectangle
   attr_accessor :point1, :point2, :point3, :point4
 
-  def self.new point1, point2, point3, point4
-    line1=Line.new point1, point2
-    line2=Line.new point2, point3
-    line3=Line.new point3, point4
-    line4=Line.new point4, point1
-
-    return nil unless (
-      (Line.orthogonal? line1, line2) and
-      (Line.orthogonal? line3, line4) and
-      (line1.length == line3.length) and
-      (line2.length == line4.length)
-    )
-    super
+  def self.new_with_sides point1, length, breadth
+    point2 = Point.new(point1.x + length, point1.y)
+    point3 = Point.new(point1.x, point1.y + breadth)
+    point4 = Point.new(point2.x, point3.y )
+    Rectangle.new point1, point2, point3, point4
   end
 
-  def initialize point1, point2, point3, point4
-    @point1 = point1
-    @point2 = point2
-    @point3 = point3
-    @point4 = point4
+  def new point1, point2, point3, point4
+    super if valid? point1, point2, point3, point4
+  end
+
+  def self.valid? point1, point2, point3, point4
+    line1 = Line.new point1, point2
+    line2 = Line.new point2, point3
+    line3 = Line.new point3, point4
+    line4 = Line.new point4, point1
+
+    (
+      Line.orthogonal?(line1, line2) and Line.orthogonal?(line3, line4) and
+      (line1.length == line3.length) and (line2.length == line4.length)
+    )
+  end
+
+  def initialize *arg
+    @point1 = arg[0]
+    @point2 = arg[1]
+    @point3 = arg[2]
+    @point4 = arg[3]
   end
 
   def area
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 describe 'Rectangle' do
-  it 'should declare a rectangle with four points' do
+  it 'should declare a rectangle with variable and valid inputs' do
     point1 = Point.new 0, 0
     point2 = Point.new 2, 0
     point3 = Point.new 2, 4
     point4 = Point.new 0, 4
+    point2_new = Point.new 3, 0
+    point3_new = Point.new 0, 4
+    point4_new = Point.new 3, 4
+    rectangle_new = Rectangle.new point1, 3, 4
     rectangle= Rectangle.new point1, point2, point3, point4
     expect(Point.equal? rectangle.point1, point1).to eq(true)
     expect(Point.equal? rectangle.point2, point2).to eq(true)
     expect(Point.equal? rectangle.point3, point3).to eq(true)
     expect(Point.equal? rectangle.point4, point4).to eq(true)
+    expect(Point.equal? rectangle_new.point1, point1).to eq(true)
+    expect(Point.equal? rectangle_new.point2, point2_new).to eq(true)
+    expect(Point.equal? rectangle_new.point3, point3_new).to eq(true)
+    expect(Point.equal? rectangle_new.point4, point4_new).to eq(true)
   end
 
   it 'should not declare a rectangle with four invalid points' do

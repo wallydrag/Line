241426f6a99868125c778b810565714d7d9f17f0
wallydrag <debashish.ghatak@gmail.com>
2015-05-27 14:56:16 +0530

    [Deba and Chukla] rectangle with new sides completed, valid modified with tests

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -8,16 +8,24 @@ class Rectangle
     Rectangle.new point1, point2, point3, point4
   end
 
-  def new point1, point2, point3, point4
-    super if valid? point1, point2, point3, point4
+  def self.new point1, point2, point3, point4
+    return nil unless valid? point1, point2, point3, point4
+    super
   end
 
   def self.valid? point1, point2, point3, point4
-    line1 = Line.new point1, point2
-    line2 = Line.new point2, point3
-    line3 = Line.new point3, point4
-    line4 = Line.new point4, point1
-
+    line1_tmp = Line.new point1, point2
+    line2_tmp = Line.new point2, point3
+    line3_tmp = Line.new point3, point4
+    line4_tmp = Line.new point4, point1
+    line5_tmp = Line.new point3, point1
+    line6_tmp = Line.new point4, point2
+    lines = [line1_tmp, line2_tmp, line3_tmp, line4_tmp, line5_tmp, line6_tmp]
+    lines = lines.sort {|x, y| x.length <=> y.length}
+    line1 = lines[0]
+    line2 = lines[2]
+    line3 = lines[1]
+    line4 = lines[3]
     (
       Line.orthogonal?(line1, line2) and Line.orthogonal?(line3, line4) and
       (line1.length == line3.length) and (line2.length == line4.length)
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -6,28 +6,58 @@ describe 'Rectangle' do
     point2 = Point.new 2, 0
     point3 = Point.new 2, 4
     point4 = Point.new 0, 4
-    point2_new = Point.new 3, 0
-    point3_new = Point.new 0, 4
-    point4_new = Point.new 3, 4
-    rectangle_new = Rectangle.new point1, 3, 4
     rectangle= Rectangle.new point1, point2, point3, point4
     expect(Point.equal? rectangle.point1, point1).to eq(true)
     expect(Point.equal? rectangle.point2, point2).to eq(true)
     expect(Point.equal? rectangle.point3, point3).to eq(true)
     expect(Point.equal? rectangle.point4, point4).to eq(true)
-    expect(Point.equal? rectangle_new.point1, point1).to eq(true)
-    expect(Point.equal? rectangle_new.point2, point2_new).to eq(true)
-    expect(Point.equal? rectangle_new.point3, point3_new).to eq(true)
-    expect(Point.equal? rectangle_new.point4, point4_new).to eq(true)
   end
 
   it 'should not declare a rectangle with four invalid points' do
-    point1 = Point.new 0, 0
-    point2 = Point.new 2, 0
-    point3 = Point.new 3, 4
-    point4 = Point.new 0, 4
+    point1 = Point.new 4, 0
+    point2 = Point.new 0, 0
+    point3 = Point.new 0, 4
+    point4 = Point.new 3, 4
     rectangle = Rectangle.new point1, point2, point3, point4
     expect(rectangle).to eq(nil)
+  end
+
+  describe 'Rectangle with sides' do
+    it 'should declare a rectangle with a point and 2 side lengths' do
+      point = Point.new 0, 0
+      length = 3
+      breadth = 4
+      rectangle = Rectangle.new_with_sides point, length, breadth
+      expect(Point.equal? rectangle.point1, point).to eq(true)
+      expect(Point.equal? rectangle.point2, Point.new(point.x + length, point.y)).to eq(true)
+      expect(Point.equal? rectangle.point3, Point.new(point.x, point.y + breadth)).to eq(true)
+      expect(Point.equal? rectangle.point4, Point.new(point.x + length, point.y + breadth)).to eq(true)
+    end
+  end
+
+  describe 'is valid' do
+      point1 = Point.new 3, 0
+      point2 = Point.new 0, 0
+      point3 = Point.new 0, 4
+
+    it 'should return true for valid rectangle points' do
+      point4 = Point.new 3, 4
+      expect(Rectangle.valid? point1, point2, point3, point4).to eq(true)
+    end
+
+    it 'should return false for invalid rectangle points' do
+      point4 = Point.new 39, 49
+      expect(Rectangle.valid? point1, point2, point3, point4).to eq(false)
+    end
+
+    it 'should be valid' do
+      point1 = Point.new 0,0
+      point2 = Point.new 3,0
+      point3 = Point.new 0,4
+      point4 = Point.new 3,4
+      puts Rectangle.valid? point1, point2, point3, point4
+      expect(Rectangle.valid? point1, point2, point3, point4).to eq(true)
+    end
   end
 
   describe 'area' do
